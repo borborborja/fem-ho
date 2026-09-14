1c168f1d024bc7af55bdb0da33167dfe1ac65bce
borborborja <borborbor@users.noreply.github.com>
2026-09-14 12:55:41 +0000

    chore: prepara la release 0.18.1

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "ho.fem"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "0.18.0"
+        versionCode = 19
+        versionName = "0.18.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
